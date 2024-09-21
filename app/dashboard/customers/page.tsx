29dd558b495f00b6b86dff48f43e4befc90ae3ef
Gustav-i <112987943+Gustav-i@users.noreply.github.com>
2024-09-21 10:30:24 +0200

    update

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,5 +1,19 @@
+import { Metadata } from 'next'
+import { lusitana } from "@/app/ui/fonts"
+ 
+
+export const metadata: Metadata = {
+  title: 'Customers',
+}
+
 
 export default function Page() 
 {
-  return <p>Customers Page</p>
+  return (
+    <div className="flex w-full items-center justify-between">
+      <h1 className={`${lusitana.className} text-2xl`}>
+        Customers Page 
+      </h1>
+    </div>
+  )
 }
