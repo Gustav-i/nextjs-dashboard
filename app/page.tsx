8c5d0f2a3681dea138b90578d7842309fe13d6b2
Gustavo Garcia Samartano <gustavosammartano@gmail.com>
2024-10-23 17:34:31 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import AcmeLogo from '@/app/ui/acme-logo'
 import { ArrowRightIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import styles from '@/app/ui/home.module.css'
+import Image from 'next/image'
 
 
-export default function Page() 
+export default function Home() 
 {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -21,14 +22,27 @@ export default function Page()
             , brought to you by Vercel.
           </p>
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            <span>Dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={500}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
       <div className={styles.shape} />
